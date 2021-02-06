40f551feb66a14fcb4c017c7f6c07cdedb3b349f
Cristian Acosta <Cristian33244@gmail.com>
2021-02-06 14:47:28 -0500

    parrafo y color de titulo

--- empresa_A.php
+++ empresa_A.php
@@ -4,8 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>MI empresa</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <h1>Hola </h1>
+
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent quis tempus dui. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Morbi venenatis sagittis efficitur. Nunc scelerisque porta sapien sit amet facilisis. Donec tincidunt mattis nunc, in molestie erat ultricies et. Quisque sed facilisis urna. Duis ornare purus vel lorem vehicula consectetur. Aenean vitae finibus mi. Donec egestas neque vel porttitor maximus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Donec sit amet lacinia leo, ut mollis lectus.</p>
 </body>
 </html>
